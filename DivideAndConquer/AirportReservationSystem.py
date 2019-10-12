25146a59cffd728d55a176f54f22010559017bb0
Quy Tran <quytran288@gmail.com>
2019-10-11 23:33:06 -0700

    Airport Reservation System

--- DivideAndConquer/AirportReservationSystem.py
+++ DivideAndConquer/AirportReservationSystem.py
@@ -10,12 +10,8 @@ Ideas:
 - Solve this problem using BST data structure
     - Search BST for place to insert: O(Lg n)
     - Insert: O(1)
-
-- How many planes landed before t?
-    - Walk down tree to find desired time
-    - Add in 1 for the nodes that are smaller
-    - Add in the sub-tree size to the left of just compared node
 """
+import unittest
 
 
 class TreeNode:
@@ -106,13 +102,36 @@ def assign_num_of_below_nodes(node):
     return node.numOfBelowNodes
 
 
-nums = [42, 60, 65, 70, 80]
-node = build_bst_from_sorted_list(nums)
+def get_num_of_plane_landed_before(node, time, res):
+    """
+    Walk down tree to find desired time
+    Add in 1 for the nodes that are smaller
+    Add in the sub-tree size to the left of just compared node
+    """
+    if not node:
+        return res
 
-# traverse_bst(node)
+    if time < node.val:
+        return get_num_of_plane_landed_before(node.left, time, res)
 
-reserve_landing(node, 50, 3)
-reserve_landing(node, 46, 3)
-reserve_landing(node, 41, 3)
-assign_num_of_below_nodes(node)
-traverse_bst(node)
+    nums_nodes_below_left_node = node.left.numOfBelowNodes if node.left else 0
+    return get_num_of_plane_landed_before(node.right, time, res + 1 + nums_nodes_below_left_node)
+
+
+class Test(unittest.TestCase):
+    def test_normal(self):
+        nums = [42, 60, 65, 70, 80]
+        node = build_bst_from_sorted_list(nums)
+
+        reserve_landing(node, 50, 3)
+        reserve_landing(node, 46, 3)
+        # won't added to BST
+        reserve_landing(node, 41, 3)
+        assign_num_of_below_nodes(node)
+        # traverse_bst(node)
+
+        self.assertEqual(get_num_of_plane_landed_before(node, 71, 0), 6, "Should return a correct result")
+        self.assertEqual(get_num_of_plane_landed_before(node, 62, 0), 4, "Should return a correct result")
+        self.assertEqual(get_num_of_plane_landed_before(node, 90, 0), 7,
+                         "Should return number of nodes for time greater than all times in BST")
+        self.assertEqual(get_num_of_plane_landed_before(node, 40, 0), 0, "Should return 0 for time that less than all times in BST")
