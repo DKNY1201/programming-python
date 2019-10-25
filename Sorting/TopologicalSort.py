f1bd418021fd200dc53e3b7479ea9c0c4191cb7c
Quy Tran <quytran288@gmail.com>
2019-10-24 23:12:25 -0700

    Topological sort

---
+++ Sorting/TopologicalSort.py
@@ -0,0 +1,66 @@
+import unittest
+
+
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_edge(self, u, v):
+        if u not in self.adj_list.keys():
+            self.adj_list[u] = []
+
+        self.adj_list[u].append(v)
+
+    def add_vertex(self, u):
+        self.adj_list[u] = []
+
+    def topological_sort(self):
+        visited = set()
+        stack = []
+
+        for node in self.adj_list.keys():
+            if node not in visited:
+                self.helper(visited, stack, node)
+
+        stack.reverse()
+
+        return stack
+
+    def helper(self, visited, stack, node):
+        visited.add(node)
+        # DFS traversal
+        if node in self.adj_list.keys():
+            for adj_node in self.adj_list[node]:
+                if adj_node not in visited:
+                    self.helper(visited, stack, adj_node)
+
+        # when all adjacent nodes are visited, add it to stack
+        stack.append(node)
+
+
+class Test(unittest.TestCase):
+
+    def test_topological_sort(self):
+        graph = Graph()
+        graph.add_edge("G", "H")
+        graph.add_edge("A", "H")
+        graph.add_edge("A", "B")
+        graph.add_edge("B", "C")
+        graph.add_edge("C", "F")
+        graph.add_edge("D", "E")
+        graph.add_edge("E", "F")
+        graph.add_vertex("I")
+        expected_result = ["I", "D", "E", "A", "B", "C", "F", "G", "H"]
+        self.assertEqual(graph.topological_sort(), expected_result,
+                         "Should return correct result for string topological sort")
+
+        graph1 = Graph()
+        graph1.add_edge(5, 0)
+        graph1.add_edge(5, 2)
+        graph1.add_edge(2, 3)
+        graph1.add_edge(3, 1)
+        graph1.add_edge(4, 0)
+        graph1.add_edge(4, 1)
+        expected_result_1 = [4, 5, 2, 3, 1, 0]
+        self.assertEqual(graph1.topological_sort(), expected_result_1,
+                         "Should return correct result for number topological sort")
