b39da1f689f3275141fa4226b65d4224b2627a96
Quy Tran <quytran288@gmail.com>
2019-11-10 23:14:21 -0800

    Improved Critical Routers problem - to find critical routers

--- Graph/CritialRouters.py
+++ Graph/CritialRouters.py
@@ -94,6 +94,80 @@ def dfs(start_router, prevent_router, routers, num_routers):
     return len(visited) < num_routers - 1
 
 
+class Graph:
+    def __init__(self, num_routers):
+        self.num_routers = num_routers + 1
+        self.adj_list = [[] for i in range(self.num_routers)]
+        # time when examine a vertex
+        self.time = 0
+
+    def add_edge(self, u, v):
+        self.adj_list[u].append(v)
+        self.adj_list[v].append(u)
+
+    def find_critical_router_tarjan(self):
+        """
+        A vertex is critical vertex iff it satisfies one of following condition
+        - If vertex is root and has more than 2 children
+        - If vertex is not root and no back edges from its children to its ancestors
+
+        Note:
+        - To have ancestor -> descendant, use DFS
+        - To identify a back edge, store the time we examine a vertex. If we encounter a vertex v again from a vertex u
+        then there is a back edge from u -> v
+        """
+        if not self.adj_list:
+            return []
+
+        # keep track visited vertex
+        visited = [0] * self.num_routers
+        # keep track time we examine a vertex. ids never change after assign a value
+        ids = [float("Inf")] * self.num_routers
+        # keep track lowest time of reachable vertex via back edge
+        lows = [float("Inf")] * self.num_routers
+        # keep track parent of a vertex, root has no parent
+        parents = [0] * self.num_routers
+        parents[0] = -1
+        # result to return
+        res = [0] * self.num_routers
+
+        for i in range(self.num_routers):
+            if not visited[i]:
+                self.dfs(i, visited, ids, lows, parents, res)
+
+        return [idx for idx, i in enumerate(res) if i == 1]
+
+    def dfs(self, u, visited, ids, lows, parents, res):
+        visited[u] = 1
+        ids[u] = self.time
+        lows[u] = self.time
+        num_child = 0
+        # increase time for examining next vertex
+        self.time += 1
+
+        for v in self.adj_list[u]:
+            num_child += 1
+
+            if not visited[v]:
+                parents[v] = u
+                self.dfs(v, visited, ids, lows, parents, res)
+                # after examine its children, update lows of current vertex
+                lows[u] = min(lows[u], lows[v])
+
+                # if root
+                if parents[u] == -1:
+                    # has more than 2 children
+                    if num_child >= 2:
+                        res[u] = 1
+                # else if current examine time less than low of its children. In other words, no back edge
+                elif ids[u] <= lows[v]:
+                    res[u] = 1
+
+            elif v != parents[u]:
+                # update lows of current vertex
+                lows[u] = min(lows[u], ids[v])
+
+
 class Test(unittest.TestCase):
     def test_get_max(self):
         num_routers = 7
@@ -114,4 +188,46 @@ class Test(unittest.TestCase):
         num_routers = 0
         links = []
         self.assertEqual([], get_critical_routers(num_routers, links),
+                         "Should return empty list of critical routers when there is no router")
+
+    def test_find_critical_router_tarjan(self):
+        graph = Graph(7)
+        graph.add_edge(0, 1)
+        graph.add_edge(0, 2)
+        graph.add_edge(1, 2)
+        graph.add_edge(2, 3)
+        graph.add_edge(3, 4)
+        graph.add_edge(2, 5)
+        graph.add_edge(5, 6)
+        graph.add_edge(6, 7)
+        graph.add_edge(5, 7)
+        self.assertEqual(sorted([2, 3, 5]), sorted(graph.find_critical_router_tarjan()),
+                         "Should return correct list of critical routers")
+
+        graph = Graph(6)
+        graph.add_edge(0, 1)
+        graph.add_edge(0, 2)
+        graph.add_edge(1, 3)
+        graph.add_edge(2, 3)
+        graph.add_edge(2, 5)
+        graph.add_edge(5, 6)
+        graph.add_edge(3, 4)
+        self.assertEqual(sorted([2, 3, 5]), sorted(graph.find_critical_router_tarjan()),
+                         "Should return correct list of critical routers")
+
+        graph = Graph(5)
+        graph.add_edge(0, 1)
+        graph.add_edge(1, 2)
+        graph.add_edge(2, 3)
+        graph.add_edge(3, 4)
+        self.assertEqual(sorted([1, 2, 3]), sorted(graph.find_critical_router_tarjan()),
+                         "Should return correct list of critical routers when all routers are on a line")
+
+        graph = Graph(2)
+        graph.add_edge(0, 1)
+        self.assertEqual([], sorted(graph.find_critical_router_tarjan()),
+                         "Should return empty list of critical routers when there is only 2 router")
+
+        graph = Graph(0)
+        self.assertEqual([], sorted(graph.find_critical_router_tarjan()),
                          "Should return empty list of critical routers when there is no router")
