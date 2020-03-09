e1a9d52063eed88ce63a1b14c525c19f1fbebd5d
Quy Tran <quytran288@gmail.com>
2020-03-08 23:35:02 -0700

    0/1 Knapsack bottom up

--- DP/Knapsack.py
+++ DP/Knapsack.py
@@ -25,6 +25,20 @@ def helper(W, V, k, i, dp):
     return res
 
 
+def knapsack_bottom_up(W, V, k):
+    n = len(W)
+    dp = [[0] * (k + 1) for i in range(n)]
+
+    for i in range(0, n):
+        for j in range(k + 1):
+            if j == 0:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = max(dp[i - 1][j], (dp[i - 1][j - W[i]] if j - W[i] >= 0 else 0) + V[i])
+
+    return dp[-1][-1]
+
+
 class Test(unittest.TestCase):
     def test_knapsack(self):
         W = [1, 2, 3, 2, 5]
@@ -41,3 +55,19 @@ class Test(unittest.TestCase):
         V = [60, 100, 120]
         k = 50
         self.assertEqual(220, knapsack(W, V, k), "Should return maximum value we can store in knapsack")
+
+    def test_knapsack_bottom_up(self):
+        W = [1, 2, 3, 2, 5]
+        V = [5, 3, 5, 3, 2]
+        k = 10
+        self.assertEqual(16, knapsack_bottom_up(W, V, k), "Should return maximum value we can store in knapsack")
+
+        W = [1, 12, 3, 4, 5, 2, 10]
+        V = [15, 3, 5, 3, 10, 5, 6]
+        k = 20
+        self.assertEqual(38, knapsack_bottom_up(W, V, k), "Should return maximum value we can store in knapsack")
+
+        W = [10, 20, 30]
+        V = [60, 100, 120]
+        k = 50
+        self.assertEqual(220, knapsack_bottom_up(W, V, k), "Should return maximum value we can store in knapsack")
